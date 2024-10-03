c349773fffb800a7f8ac57b57451b5621ec1ede0
SHANEEISLEYL <eisleylagrimas329@gmail.com>
2024-10-03 14:07:00 +0800

    Implemented Student List

--- AngularBasicRouting/src/app/app.routes.ts
+++ AngularBasicRouting/src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+
+// Export the routes constant
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'homepage', component: HomepageComponent },
+  { path: 'landing-page', component: LandingPageComponent },
+  { path: '', redirectTo: '/landing-page', pathMatch: 'full' },
+  { path: '**', redirectTo: '/landing-page' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutesModule { }
